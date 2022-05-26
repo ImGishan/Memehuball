1c6d5b17ba945266f398ecb459dfe93ded0701f4
Gishan Vithana <105406186+ImGishan@users.noreply.github.com>
2022-05-26 11:24:21 +0530

    Update cal.py

--- plugins/cal.py
+++ plugins/cal.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 from .config import CALCULATE_TEXT, CALCULATE_BUTTONS
 
-@Client.on_message(filters.command(["calc", "calculate", "calculator"]))
+@Client.on_message(filters.command(["cal", "calc", "calculate", "calculator"]))
 async def calculate(bot, update):
     await update.reply_text(
         text=CALCULATE_TEXT,
