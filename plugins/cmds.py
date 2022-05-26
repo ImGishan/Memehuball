1ff40a3cd699170c1e2de202e505ee841354b3c1
Gishan Vithana <105406186+ImGishan@users.noreply.github.com>
2022-05-26 12:04:16 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -62,8 +62,31 @@ async def help(bot, message):
         reply_markup=CLOSE_BUTTON,
         disable_web_page_preview=True
          )
-         
+#menu Regex         
+  
+@Client.on_message(filters.private & filters.regex(pattern="🤴 OWNER 🤴"))   
+async def startprivate(bot, message):
+     await bot.send_sticker(message.chat.id, random.choice(OWNER_STICKER),reply_markup=OWNER_BTN
+     )
+@Client.on_message(filters.private & filters.regex(pattern="💻 Bot Devs 💻"))   
+async def startprivate(bot, message):
+     await bot.send_sticker(message.chat.id, random.choice(DEV_STICKER),reply_markup=DEV_BTN
+     )
+
+@Client.on_message(filters.private & filters.regex(pattern="👮‍♂️ MemeHub Admins 👮‍♂️"))   
+async def startprivate(bot, message):
+     await bot.send_sticker(message.chat.id, random.choice(ADMIN_STICKER),reply_markup=ADMIN_BTN
+     )
+    
+@Client.on_message(filters.private & filters.regex(pattern="NEXT 🔜"))   
+async def startprivate(bot, message):
+     await bot.send_message(message.chat.id, text='NEXT 🔜',reply_markup=NEXT_1
+     )
         
+@Client.on_message(filters.private & filters.regex(pattern="BACK 🔙"))   
+async def startprivate(bot, message):
+     await bot.send_message(message.chat.id, text='BACK 🔙',reply_markup=REPLY_BTN
+     )
                        
 print("cmds.py Working....")
 
