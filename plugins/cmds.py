5b7048433c467c6ef1edb0bce90af8ae86f18acf
Gishan Vithana <105406186+ImGishan@users.noreply.github.com>
2022-05-26 11:39:25 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -37,8 +37,34 @@ async def startprivate(bot, message):
         disable_web_page_preview=True,
         quote=True
     )
-    
 
+@Client.on_message(filters.command(["help", "help@MemeHubTgSl_Bot"]))
+async def startprivate(bot, message):
+    if force_subchannel:
+        try:
+            user = await bot.get_chat_member(force_subchannel, message.from_user.id)
+            if user.status == "kicked out":
+                await message.reply_text("Yourt Banned")
+                return 
+        except UserNotParticipant:
+            file_id = "CAADBQADOAcAAn_zKVSDCLfrLpxnhAI"
+            await bot.send_sticker(message.chat.id, file_id)
+            text = FORCESUB_TEXT
+            reply_markup = FORCESUB_BUTTONS
+            await message.reply_text(
+            text=text,
+            reply_markup=reply_markup
+            ) 
+            return
+    await bot.send_sticker(message.chat.id, random.choice(HELP_STICKER))
+    await message.reply_text(
+        text=HELP_STRING,
+        reply_markup=CLOSE_BUTTON,
+        disable_web_page_preview=True
+         )
+         
+         
+print("cmds.py Working....")
 
 
 
